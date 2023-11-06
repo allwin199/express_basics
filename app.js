bcafdc3400786e224204985277dc159ec1b7f2ed
worksofallwin <princeallwin199@gmail.com>
2023-11-06 16:15:35 +0530

    add styles to html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -9,11 +10,11 @@ const productRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(productRoutes);
 
 app.use("/", (req, res) => {
-    res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
