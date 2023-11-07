58699e6659ed6de4d9a3dab43f96641835fadcce
worksofallwin <princeallwin199@gmail.com>
2023-11-07 10:29:10 +0530

    complete pug template

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 // Routes
-const adminRoutes = require("./routes/admin");
-const productRoutes = require("./routes/shop");
+const adminData = require("./routes/admin");
+const shop = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); // public dir is given read access
 
-app.use("/admin", adminRoutes);
-app.use(productRoutes);
+app.use("/admin", adminData.routes);
+app.use(shop);
 
 app.use("/", (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404");
 });
 
 app.listen(3000);
