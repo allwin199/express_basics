4f912ca2b9a675f0989e1993f3896e2aed50f972
worksofallwin <princeallwin199@gmail.com>
2023-12-01 19:18:10 +0530

    complete edit and delete cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -57,18 +57,55 @@ exports.getCart = (req, res) => {
 
 exports.postCart = (req, res) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
-        Cart.addProduct(prodId, product.price);
-        res.redirect("/cart");
-    });
+    let fetchedCart;
+    req.user
+        .getCart()
+        .then((cart) => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: prodId } });
+        })
+        .then((products) => {
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+            }
+            let newQuantity = 1;
+            if (product) {
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return fetchedCart.addProduct(product, {
+                    through: { quantity: newQuantity },
+                });
+            }
+            return Product.findByPk(prodId)
+                .then((product) => {
+                    return fetchedCart.addProduct(product, {
+                        through: { quantity: newQuantity },
+                    });
+                })
+                .catch((err) => console.log(err));
+        })
+        .then(() => {
+            res.redirect("/cart");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
-        Cart.deleteProduct(prodId, product.price);
-        res.redirect("/cart");
-    });
+    req.user
+        .getCart()
+        .then((cart) => {
+            return cart.getProducts({ where: { id: prodId } });
+        })
+        .then((products) => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then(() => {
+            res.redirect("/cart");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res) => {
